0fa6b64b9fc1dd5692ddac1a0ac08dbbea28d178
arthur <engs-arthurkrecskovski@camporeal.edu.br>
2026-09-06 19:33:24 -0300

    tema 2 / atv 4

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function show($id)
+    {
+        return 'Exibindo aluno ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário para cadastrar aluno';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado com sucesso';
+    }
+
+    public function edit($id)
+    {
+        return 'Formulário para editar aluno ' . $id;
+    }
+
+    public function update(Request $request, $id)
+    {
+        return 'Aluno ' . $id . ' atualizado com sucesso';
+    }
+
+    public function destroy($id)
+    {
+        return 'Aluno ' . $id . ' excluído com sucesso';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
-Route::get('/', function () {
-    return view('welcome');
+// TEMA 1 - ATV 1
+
+Route::get('/sobre', function () {
+    return 'Página sobre';
 });
+
+Route::get('/alunos', [AlunoController::class, 'index']);
+
+Route::get('/contato', function () {
+    return 'Página de contato';
+});
+
+
+// TEMA 1 - ATV 2
+
+Route::get('/produto/{id}', function ($id) {
+    return 'Produto: ' . $id;
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return 'Categoria: ' . $id;
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return 'Usuário: ' . $id;
+});
+
+
+// TEMA 2 - ATV 4
+
+Route::get('/alunos/create', [AlunoController::class, 'create']);
+
+Route::post('/alunos', [AlunoController::class, 'store']);
+
+Route::get('/alunos/{id}', [AlunoController::class, 'show']);
+
+Route::get('/alunos/{id}/edit', [AlunoController::class, 'edit']);
+
+Route::put('/alunos/{id}', [AlunoController::class, 'update']);
+
+Route::delete('/alunos/{id}', [AlunoController::class, 'destroy']);
