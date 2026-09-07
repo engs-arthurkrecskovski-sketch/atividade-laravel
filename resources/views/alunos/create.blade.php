52b685885e07f7fe9cf71d33f3eb61e6cad9f118
Bruno <engs-brunoroth@camporeal.edu.br>
2026-09-07 19:03:52 -0300

    tema 8 / atv 14 - cria formulario de cadastro de aluno

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -3,7 +3,56 @@
 @section('title', 'Cadastrar Aluno')
 
 @section('content')
-    <h2>Cadastrar Aluno</h2>
+    <h2>Cadastrar aluno</h2>
 
-    <p>Aqui será realizado o cadastro de um novo aluno.</p>
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $erro)
+                <li>{{ $erro }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form action="{{ url('/alunos') }}" method="POST">
+        @csrf
+
+        <p>
+            <label for="nome">Nome:</label>
+            <input
+                type="text"
+                id="nome"
+                name="nome"
+                value="{{ old('nome') }}"
+                maxlength="255"
+                required
+            >
+        </p>
+
+        <p>
+            <label for="email">Email:</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                value="{{ old('email') }}"
+                maxlength="255"
+                required
+            >
+        </p>
+
+        <p>
+            <label for="curso">Curso:</label>
+            <input
+                type="text"
+                id="curso"
+                name="curso"
+                value="{{ old('curso') }}"
+                maxlength="255"
+                required
+            >
+        </p>
+
+        <button type="submit">Cadastrar</button>
+        <a href="{{ url('/alunos') }}">Cancelar</a>
+    </form>
 @endsection
